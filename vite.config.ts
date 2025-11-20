68a216b5ab81fe2a154e81ad2ff5d90856d4d355
raflores2310 <raflores2310@gmail.com>
2025-11-20 03:02:46 +0000

    Mejoras pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist'
-  }
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Dulce Crepa POS',
+        short_name: 'DulceCrepa',
+        description: 'Un sistema de punto de venta para la crepería Dulce Crepa.',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#D9538A',
+        theme_color: '#D9538A',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
